8022739e9568c5c441cf3f73fabc24f9af3a761e
Jonathan Smith <jdasmith@gmail.com>
2016-06-01 18:26:49 +0100

    visitLauncherEle upgraded
    Now can produce arbitrary xy scatter plots, scripted from outside visit.

--- ConversionScripts/ElegantToVisIt/visitLauncherEle.py
+++ ConversionScripts/ElegantToVisIt/visitLauncherEle.py
@@ -23,35 +23,88 @@ def SetupPhase2():
   ce=visit.OpenComputeEngine(phase2)
   return phase2
 
+def SaveWindow(filename):
+  SaveWindowAtts = visit.SaveWindowAttributes()
+  SaveWindowAtts.outputToCurrentDirectory = 1
+  SaveWindowAtts.outputDirectory = "."
+  SaveWindowAtts.fileName = filename
+  SaveWindowAtts.family = 0
+  SaveWindowAtts.format = SaveWindowAtts.PNG  # BMP, CURVE, JPEG, OBJ, PNG, POSTSCRIPT, POVRAY, PPM, RGB, STL, TIFF, ULTRA, VTK, PLY
+  SaveWindowAtts.width = 1024
+  SaveWindowAtts.height = 1024
+  SaveWindowAtts.screenCapture = 0
+  SaveWindowAtts.saveTiled = 0
+  SaveWindowAtts.quality = 80
+  SaveWindowAtts.progressive = 0
+  SaveWindowAtts.binary = 0
+  SaveWindowAtts.stereo = 0
+  SaveWindowAtts.compression = SaveWindowAtts.PackBits  # None, PackBits, Jpeg, Deflate
+  SaveWindowAtts.forceMerge = 0
+  SaveWindowAtts.resConstraint = SaveWindowAtts.ScreenProportions  # NoConstraint, EqualWidthHeight, ScreenProportions
+  SaveWindowAtts.advancedMultiWindowSave = 0
+  visit.SetSaveWindowAttributes(SaveWindowAtts)
+  visit.SaveWindow()
+
+
 # either load parameters from file, or enter them afresh using serialize/pickle
 # LocalSettings
-localVisItDir= "/home/jonny/visit2_10_0.linux-x86_64/2.10.0"
+localVisItDir = "/home/jonny/visit2_10_0.linux-x86_64"
 localPythonPackageDir = "/home/jonny/visit2_10_0.linux-x86_64/2.10.0/linux-x86_64/lib/site-packages" 
-runRemotely=0
+runRemotely=1
 remoteTimeSeriesAstraDB='phase2.wonder.hartree.stfc.ac.uk:/gpfs/stfc/local/HCP084/bwm06/shared/testastra/working/test_analyzeBeam.h5'
 remoteTimeSeriesEleSigmaDB='phase2.wonder.hartree.stfc.ac.uk:/gpfs/stfc/local/HCP084/bwm06/shared/testele/phase100/clara.sig-page1.h5'
-remoteTimeSeriesElePhaseSpaceDB='phase2.wonder.hartree.stfc.ac.uk:/gpfs/stfc/local/HCP084/bwm06/shared/testele/clara_track_electrons_*.vsh5'
+remoteElePhaseSpaceDB='phase2.wonder.hartree.stfc.ac.uk:/gpfs/stfc/local/HCP084/bwm06/shared/testele/clara_track_electrons_*.vsh5 database'
+#remoteElePhaseSpaceDB='phase2.wonder.hartree.stfc.ac.uk:/gpfs/stfc/local/HCP084/bwm06/shared/testele/clara_track_electrons_1.vsh5'
 #Remote settings
 def isOpenDatabase(filename):
   pass
   return 0
 
 def PhaseSpace(x1,x2,dumpNo):
+   visit.AddWindow()
    ScatAttrs = visit.ScatterAttributes()
-   ScatAtts.var1 = x1
-   ScatAtts.var2 = x2
-   ScatAtts.var1Role = ScatterAtts.Coordinate0
-   ScatAtts.var2Role = ScatterAtts.Coordinate1
-   ScatAtts.var3Role = ScatterAtts.None
-   ScatAtts.var4Role = ScatterAtts.None
-   visit.AddPlot('Scatter',x1,1,1) 
+   ScatAttrs.var1 = x1
+   ScatAttrs.var2 = x2
+#   ScatAttrs.var1Role = visit.ScatterAtts.Coordinate0
+#   ScatAttrs.var2Role = visit.ScatterAtts.Coordinate1
+#   ScatAttrs.var3Role = visit.ScatterAtts.None
+#   ScatAttsr.var4Role = visit.ScatterAtts.None
+   ScatAttrs.SetVar1Role(0) #coord 0
+   ScatAttrs.SetVar2Role(1) #coord 1
+   ScatAttrs.SetVar3Role(4) #none
+   ScatAttrs.SetVar4Role(4) #none   
+   ScatAttrs.scaleCube = 0
+   visit.AddPlot('Scatter',x1,1,1)
+   visit.SetPlotOptions(ScatAttrs)
+   visit.SetTimeSliderState(dumpNo)
+   visit.DrawPlots() 
 
 def PhaseSpaceXY(elementNo=1):
-  if isOpenDataBase(''):
-    PhaseSpace('electrons_x','electrons_y')
+  if isOpenDatabase(''):
+    PhaseSpace('electrons_x','electrons_y',1)
   else:
-    psData=visit.OpenDatabase(,elementNo,'Vs')
-    PhaseSpace('electrons_x','electrons_y')
+#    psData=visit.OpenDatabase(remoteElePhaseSpaceDB,elementNo,'Vs')
+    psData=visit.OpenDatabase(remoteElePhaseSpaceDB,0,'Vs')
+    PhaseSpace('electrons_x','electrons_y',elementNo)
+
+def TimeSeriesS1():
+  visit.AddPlot('Curve','s1')
+  visit.AddPlot('Curve','s2')
+  visit.DrawPlots()
+  visit.AddWindow()
+  visit.AddPlot('Curve','pAverage')
+  visit.DrawPlots()
+  visit.SetActiveWindow(1)
+  AnnotationAtts = visit.AnnotationAttributes()
+  AnnotationAtts.axes2D.yAxis.grid = 1
+  AnnotationAtts.axes2D.xAxis.grid = 1
+  AnnotationAtts.axes2D.xAxis.title.userTitle = 1
+  AnnotationAtts.axes2D.xAxis.title.userUnits = 1
+  AnnotationAtts.axes2D.xAxis.title.title = "Position along the machine s"
+  AnnotationAtts.axes2D.xAxis.title.units = "m"
+  visit.SetAnnotationAttributes(AnnotationAtts)
+  visit.DrawPlots()
+
     
 import sys
 sys.path.insert(0,localPythonPackageDir)
@@ -59,22 +112,18 @@ import visit
 visit.Launch(vdir=localVisItDir)
 if runRemotely:
   p2=SetupPhase2()
-  data2=visit.OpenDatabase(remoteTimeSeriesAstraDB,0,'Vs')
+#  data2=visit.OpenDatabase(remoteTimeSeriesAstraDB,0,'Vs')
+  data2=visit.OpenDatabase(remoteTimeSeriesEleSigmaDB,0,'Vs')
+
 else:
   data=visit.OpenDatabase
-visit.AddPlot('Curve','s1')
-visit.AddPlot('Curve','s2')
-visit.DrawPlots()
-visit.AddWindow()
-visit.AddPlot('Curve','pAverage')
-visit.DrawPlots()
-visit.SetActiveWindow(1)
-AnnotationAtts = visit.AnnotationAttributes()
-AnnotationAtts.axes2D.yAxis.grid = 1
-AnnotationAtts.axes2D.xAxis.grid = 1
-AnnotationAtts.axes2D.xAxis.title.userTitle = 1
-AnnotationAtts.axes2D.xAxis.title.userUnits = 1
-AnnotationAtts.axes2D.xAxis.title.title = "Position along the machine s"
-AnnotationAtts.axes2D.xAxis.title.units = "m"
-visit.SetAnnotationAttributes(AnnotationAtts)
+TimeSeriesS1()
+PhaseSpaceXY(2)
+SaveWindow('phase2.png')
+PhaseSpaceXY(3)
+SaveWindow('phase3.png')
+PhaseSpaceXY(4)
+SaveWindow('phase4.png')
+
+visit.OpenCLI()
 visit.OpenGUI()
