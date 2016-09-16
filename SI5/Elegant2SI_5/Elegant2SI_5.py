Fixed fault in SI<->Elegant that casued Z-axis and units mismatch.

SI5/Elegant2SI_5/Elegant2SI_5.py
@@ -60,7 +60,7 @@ for i, row in enumerate(f):
 
 
 
-m_PZ=(5.36E-28*(ERM*1.E6)*m_P)/(np.sqrt((m_XP**2)+(m_YP**2)+1))
+m_PZ=(5.36E-28*(ERM*1.E6)*m_P)/(np.sqrt((m_XP**2)+(m_YP**2)+1.0))
 m_PX=m_PZ*m_XP
 m_PY=m_PZ*m_YP
 
@@ -68,11 +68,12 @@ m_PY=m_PZ*m_YP
 #Beta_z=np.sqrt((m_ZP**2+m_XP**2+m_YP**2)/m_ZP**2)
 
 Beta_z=m_PZ/(np.sqrt(m_PX**2+m_PY**2+m_PZ**2))
+print 'Beta_Z = ',Beta_z
 
 p_tot=np.sqrt((m_PX[:]**2)+(m_PY[:]**2)+(m_PZ[:]**2))
 Gamma=(np.sqrt(1+(p_tot/(me*c))**2))
-V_z=m_PZ/(Gamma*me)
-Z_true=V_z*m_T
+#V_z=m_PZ/(Gamma*me)
+#Z_true=V_z*m_T
 
 
 # By some reason data is flipped along Z-axis, to correct it we need to flip it again,