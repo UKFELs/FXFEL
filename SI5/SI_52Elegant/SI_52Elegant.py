Changes in SI<->Elegant, fixed units and Z-axis issue (serious fault, not yet discovered due to lack of Elegant samples and Elegant related tasks).

SI5/SI_52Elegant/SI_52Elegant.py
@@ -37,10 +37,10 @@ NE=Electrons[:,6]
 particleID=np.arange(1,len(X)+1)
 print 'Processing data...'
 
-P=(np.sqrt(Px**2+Py**2+Pz**2))/(5.36E-28*(ERM*1.E6))
+P=np.sqrt(Px**2+Py**2+Pz**2)/(5.36E-28*(ERM*1.E6))
 
 
-Beta_z=Pz/P
+Beta_z=Pz/P/(5.36E-28*(ERM*1.E6))
 
 xp=Px/Pz
 yp=Py/Pz