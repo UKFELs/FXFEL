a8aa6ed116541d7d35563f2a40e3f49582ce0dae
Piotr Traczykowski <piotr.traczykowski@strath.ac.uk>
2017-01-27 11:05:52 +0000

    Added Scaled Units only and removed old/obsolete/SI/TXT converters

---
+++ Converters_Scaled_Units/Elegant2SU/Elegant2SU.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 17 14:52:33 2015
+
+@author: piotrt
+"""
+import numpy as np
+import os
+import csv
+import sys
+import tables
+import datetime
+now = datetime.datetime.now()
+print 'Conversion time: ',now.strftime("%Y-%m-%d %H:%M:%S")
+
+ERM=0.51099906 #Electron Rest Mass
+me=9.11e-31
+e_ch=1.602e-19
+c=3e+08
+
+if len(sys.argv)==2:
+   file_name_in=sys.argv[1]
+   print 'Processing file:', file_name_in
+else:
+   print 'Usage: Elegant2SI_5 <FileName> \n'
+   sys.exit(1)  
+file_name_base  = (file_name_in.split('.')[0]).strip()
+
+
+
+
+TotalCharge=os.popen("sdds2stream -parameters=Charge %s" %(file_name_in)).read()
+
+print 'Total Charge= ',float(TotalCharge)
+os.system('sdds2stream -column=x,xp,y,yp,t,p %s > %s'%(file_name_in,file_name_base+'OUT'))
+
+linecount = sum(1 for line in open(file_name_base+'OUT'))
+
+ParticleChargePerRecord=float(TotalCharge)/linecount
+print 'Single Particle Charge= ',float(ParticleChargePerRecord)
+
+m_X = np.zeros((linecount,1), dtype=np.float64)
+m_XP = np.zeros((linecount,1), dtype=np.float64)
+m_Y = np.zeros((linecount,1), dtype=np.float64)
+m_YP = np.zeros((linecount,1), dtype=np.float64)
+m_T = np.zeros((linecount,1), dtype=np.float64)
+m_P = np.zeros((linecount,1), dtype=np.float64)
+
+
+f = csv.reader(open(file_name_base+'OUT', 'r'),delimiter=' ')
+for i, row in enumerate(f):
+ #   m[i] = float(row[0]), float(row[1]), float(row[2]), float(row[3]), float(row[4]), float(row[5]), float(row[6])
+     m_X[i] = float(row[0]),
+     m_XP[i] = float(row[1]),
+     m_Y[i] = float(row[2]),
+     m_YP[i] = float(row[3]),
+     m_T[i] = float(row[4]),
+     m_P[i] = float(row[5]),
+
+
+
+
+m_PZ=(5.36E-28*(ERM*1.E6)*m_P)/(np.sqrt((m_XP**2)+(m_YP**2)+1.0))
+m_PX=m_PZ*m_XP
+m_PY=m_PZ*m_YP
+
+#m_ZP=np.sqrt((m_P[:]**2)-(m_XP[:]**2)-(m_YP[:]**2))
+#Beta_z=np.sqrt((m_ZP**2+m_XP**2+m_YP**2)/m_ZP**2)
+
+Beta_z=m_PZ/(np.sqrt(m_PX**2+m_PY**2+m_PZ**2))
+print 'Beta_Z = ',Beta_z
+
+p_tot=np.sqrt((m_PX[:]**2)+(m_PY[:]**2)+(m_PZ[:]**2))
+Gamma=(np.sqrt(1+(p_tot/(me*c))**2))
+#V_z=m_PZ/(Gamma*me)
+#Z_true=V_z*m_T
+
+
+# By some reason data is flipped along Z-axis, to correct it we need to flip it again,
+# therefore below lines that find min Z values and rotate the beam in this point (mirror flip).
+# So e.g. beam instead of looking like <<o will look like o>>
+# MIRROR FLIP REMOVED !!! IGNORE ABOVE !!!
+
+m_Z=m_T*c*Beta_z
+mirror_flip_z=np.min(m_Z)
+m_Z_mr=m_Z-((m_Z-mirror_flip_z)*2)
+
+No_Particles_Per_Record=np.zeros((linecount,1))
+
+No_Particles_Per_Record[0:len(m_Z)]=(ParticleChargePerRecord/e_ch)
+
+#Rescale the units of momentum p/mc
+m_PX=m_PX/(me*c)
+m_PY=m_PY/(me*c)
+m_PZ=m_PZ/(me*c)
+
+x_px_y_py_z_pz_NE = np.hstack([m_X,m_PX,m_Y,m_PY,m_Z_mr,m_PZ,No_Particles_Per_Record])
+
+
+
+
+output_file=tables.open_file(file_name_base+'E2S.si5','w')
+
+# Create hdf5 file
+
+
+# Save the array into hdf5 file
+ParticleGroup=output_file.create_array('/','Particles',x_px_y_py_z_pz_NE)
+
+#Create metadata - currently for Visit to make scatter plots
+boundsGroup=output_file.create_group('/','globalGridGlobalLimits','')
+boundsGroup._v_attrs.vsType='limits'
+boundsGroup._v_attrs.vsKind='Cartesian'
+timeGroup=output_file.create_group('/','time','time')
+timeGroup._v_attrs.vsType='time'
+ParticleGroup._v_attrs.vsType='variableWithMesh'
+ParticleGroup._v_attrs.vsTimeGroup='time'
+ParticleGroup._v_attrs.vsNumSpatialDims = 3
+ParticleGroup._v_attrs.vsLimits='globalGridGlobalLimits'
+ParticleGroup._v_attrs.vsLabels='x,px,y,py,z,pz,NE'
+ParticleGroup._v_attrs.FXFELConversionTime=now.strftime("%Y-%m-%d %H:%M:%S")
+ParticleGroup._v_attrs.FXFELSourceFileOrigin='ELEGANT'
+ParticleGroup._v_attrs.FXFELSourceFileName=file_name_in
+
+#Close the file
+output_file.close()
+
+os.remove(file_name_base+'OUT')
+
+
---
+++ Converters_Scaled_Units/SU2Puffin/Experimental/SU2Puffin_HDF_ClaraOnly_Fixed_Rho.py
@@ -0,0 +1,212 @@
+# -*- coding: utf-8 -*-
+"""
+# WARNING !!#
+# THIS SCRIPT IS FOR USE ONLY FOR CLARA AIMED BEAMS WITH FIXED RHO = 0.005
+# REQUIRES TWO FILES TO WORK - ADVANCED USERS ONLY !!
+# USE THE SCRIPT FOR BEAMS AFTER MATCHING AND CONVERTING THEM FROM SDDS TO SI5/HDF
+
+Created on Tue Oct 13 17:03:14 2015
+
+@author: piotrt
+"""
+# Import necessary libraries
+import tables
+
+import numpy as np
+import gc
+import sys
+
+
+if len(sys.argv)==2:
+   file_name_in=sys.argv[1]
+   print 'Processing file:', file_name_in
+else:
+   print 'Usage: SI_52Puffin <FileName> \n'
+   sys.exit(1)  
+file_name_base  = (file_name_in.split('.')[0]).strip()
+
+# Below line is used to pull charges from original file and not from Elegant source
+# where all charges are equal and thus faulty. 
+f_help=tables.open_file('0_PF.si5','r')
+Charges=f_help.root.Particles.read()
+
+f=tables.open_file(file_name_in,'r')
+Electrons=f.root.Particles.read()
+n=len(Electrons)
+print Electrons.shape
+
+c=3.0e+8
+m=9.11e-31
+
+# Assign the data to array and descale the units from p/mc to SI !
+
+m_X = Electrons[:,0]
+m_PX = Electrons[:,1]*(m*c)
+m_Y = Electrons[:,2]
+m_PY = Electrons[:,3]*(m*c)
+m_Z = Electrons[:,4]
+m_PZ = Electrons[:,5]*(m*c)
+#m_WGHT = Electrons[:,6]
+m_WGHT = Charges[:,6
+
+del Electrons
+gc.collect()
+
+#Calculate concentration per/volume
+xyz = np.vstack([m_X,m_Y,m_Z]).T
+
+size_x=max(m_X)-min(m_X)
+size_y=max(m_Y)-min(m_Y)
+size_z=max(m_Z)-min(m_Z)
+binnumber=10
+cube_volume=(size_x*size_y*size_z)/float(binnumber**3)
+print 'Volume of sample cube: ',cube_volume
+# print 'Number of particle in record: ',num_of_particles
+print 'Size of sample x,y,z: ', size_x,size_y,size_z
+print m_WGHT.shape
+
+print (xyz.shape)
+#xyz=xyz.T
+H, edges = np.histogramdd(xyz, bins = (binnumber,binnumber,binnumber),normed=False,weights=m_WGHT.flat)
+print H.shape
+
+n_p=float(np.amax(H))/cube_volume
+print 'Np= ',n_p
+
+del xyz
+gc.collect()
+
+# Puffin variables
+e_ch=1.602e-19
+c=3.0e+8
+m=9.11e-31
+Pi=3.1415
+k_u=228.47946               # Undulator wave number default=628 k_u=2*Pi/l_w
+a_u=0.71572                     # undulator parameter ? a_u=a_w
+e_0=8.854E-12              # vacuum permitivity
+p_tot=np.sqrt((m_PX[:]**2)+(m_PY[:]**2)+(m_PZ[:]**2))
+
+#print np.size(p_tot),p_tot
+gamma=(np.sqrt(1+(p_tot/(m*c))**2))
+#print 'P_tot/mc ',p_tot/(m*c)
+#print np.size(gamma),gamma
+
+omega_p=np.sqrt((e_ch*e_ch*n_p)/(e_0*m))
+gamma_0=np.mean(gamma)
+print 'Gamma= ',gamma_0
+
+rho=(1/gamma_0)*(((a_u*omega_p)/(4*c*k_u))**(2.0/3.0))
+
+#Temporary change of rho to rho = 0.005
+rho = 0.0050
+gamma_0=456.40
+lambda_u=(2.0*Pi)/k_u
+
+# Calculated for planar undulator -> (1+(a^2)/2)
+lambda_r=(lambda_u/(2.0*gamma_0**2.0))*(1+(a_u**2.0)/2.0)
+
+print 'Rho= ', rho
+print 'Lambda_u= ',lambda_u
+print 'Lambda_r= ',lambda_r
+
+
+#Lc=1.3196758053191657E-6
+Lc=lambda_r/(4*Pi*rho)
+Lg=lambda_u/(4*Pi*rho)
+
+print 'Lg= ',Lg
+print 'Lc= ',Lc
+print '4*Pi*Rho= ',4.0*Pi*rho
+
+given_rho=0.005
+n_peak=(e_0*m/(e_ch**2.0))*((given_rho*gamma_0)**3.0)*((4.0*c*k_u)/a_u)**2.0
+#n_peak1=((e_0*m)/(e_ch**2.0))*((given_rho*gamma_0)**3.0)*(4.0*c*2.0*Pi/(lambda_u*a_u))**2.0
+print 'Peak density = ',n_p
+print 'Calculated n_peak from rho = ',n_peak
+scaled_n_peak=n_peak*Lg*(Lc**2.0)
+print 'Scaled n_peak = ',scaled_n_peak
+# Puffin output arrays:
+z2=m_Z/Lc
+
+# Find minimum of z2 and rescale z2 to start from 0.01
+min_z2=min(z2)
+
+z2=z2-min_z2+0.01
+
+x_bar=m_X[:]/(np.sqrt(Lg*Lc))
+y_bar=m_Y[:]/(np.sqrt(Lg*Lc))
+px_bar=m_PX[:]/(m*c*a_u)
+py_bar=-1.0*m_PY[:]/(m*c*a_u)
+
+
+# Centering the beam - uncomment if necessary
+#avg_x=np.mean(x_barN)
+#avg_y=np.mean(y_barN)
+
+#x_bar=avg_x-x_barN
+#y_bar=avg_y-y_barN
+
+
+
+#sig_gamma_tot
+#sig_px_bar
+#sig_py_bar
+#Ne=m_WGHT[:]/n_p          # weight charge
+Ne=m_WGHT[:]          # weight number of electrons
+
+# Scale electrons weights wit scaled_n_peak (given rho in use !!)
+Ne=Ne/scaled_n_peak
+del m_X, m_PX, m_Y, m_PY, m_Z, m_PZ, m_WGHT,p_tot 
+gc.collect()
+
+# Combine all read arrays into one
+m_Arr=np.vstack((x_bar,y_bar,z2,px_bar,py_bar,gamma/gamma_0,Ne)).T
+
+del x_bar,y_bar,px_bar,py_bar,gamma,z2,Ne
+gc.collect()
+
+output_file=tables.open_file(file_name_base+'_Puffin.hdf','w')
+# Create hdf5 file
+
+print 'Max Z2 value = ',np.max(m_Arr[:,2])
+# Save the array into hdf5 file
+ParticleGroup=output_file.create_array('/','electrons', m_Arr)
+
+#Create metadata - currently for Visit to make scatter plots
+boundsGroup=output_file.create_group('/','globalGridGlobalLimits','')
+boundsGroup._v_attrs.vsType='limits'
+boundsGroup._v_attrs.vsKind='Cartesian'
+timeGroup=output_file.create_group('/','time','time')
+timeGroup._v_attrs.vsType='time'
+ParticleGroup._v_attrs.vsType='variableWithMesh'
+ParticleGroup._v_attrs.vsTimeGroup='time'
+ParticleGroup._v_attrs.vsNumSpatialDims = 3
+ParticleGroup._v_attrs.vsLimits='globalGridGlobalLimits'
+ParticleGroup._v_attrs.vsLabels='x_bar,y_bar,z2,px_bar,py_bar,gamma,Ne'
+#Close the file
+output_file.close()
+
+m_Arr[:,6]=m_Arr[:,6]*scaled_n_peak
+#print 'Creating txt output file...'
+#out_txt=open(file_name_base+'_Puffin.txt','w')
+##
+#n=len(m_Arr)
+##
+#out_txt.write('__[x_bar]_______[y_bar]_______[px_bar]______[py_bar]_____ [Gamma]________[Z2]__________[Ne]_____'+'\n')
+#for i in range(n): 
+#        out_txt.write(      "%.6e" %(m_Arr[i,0]) + \
+#                            " % .6e" %(m_Arr[i,1]) + \
+#                            " % .6e" %(m_Arr[i,3]) + \
+#                            " % .6e" %(m_Arr[i,4]) + \
+#                            " % .6e" %(m_Arr[i,5]) + \
+#                            " % .6e" %(m_Arr[i,2]) + \
+#                            " % .6e" %(m_Arr[i,6]) + "\n")
+#out_txt.close()
+#del m_Arr
+#gc.collect()
+
+
+
+
+
+
