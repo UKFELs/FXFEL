92a4ca1b0d669486dabeb3f977b37e51dc49fbcf
Piotr Traczykowski <piotr.traczykowski@strath.ac.uk>
2017-03-27 11:20:33 +0100

    Updated VSim2SU to use array as argument - user has to pass 2 variables: name of the file to process and array name in the file that contains the data to be used.

--- Converters_Scaled_Units/VSim2SU/VSim2SU.py
+++ Converters_Scaled_Units/VSim2SU/VSim2SU.py
@@ -13,55 +13,35 @@ import sys
 c=3.0e+8
 m=9.11e-31
 
-if len(sys.argv)==2:
+if len(sys.argv)==3:
    file_name_in=sys.argv[1]
-   print 'Processing file:', file_name_in
+   array_name_in=sys.argv[2]
+   print 'Processing file:', file_name_in,' Array name is: ',array_name_in
 else:
    print 'Usage: VSim2SI_5 <FileName> \n'
    sys.exit(1)  
 file_name_base  = (file_name_in.split('.')[0]).strip()
 
 
-# Load file named 36.h5 - other name of file needs change here
-#f=h5py.File(file_name_in,'r')
+
 f=tables.open_file(file_name_in,'r')
-# Read the proper table - data from Bernard Hiddings VSim use name 'HeEletrons'
-# for array storing X,Y,Z,Px,Py,Pz,0,Weight of particle (that is their format)
-# Assign the table 'HeElectrons' to Electrons dataset/table
-Electrons=f.root.HeElectrons.read()
+
+Electrons=f.root._v_children[array_name_in].read()
 # Assign number of records (macroparticles) as n
 n=len(Electrons)
-# Create output file named output.txt
-out=open(file_name_base+'.si5','w')
-# Calculate the particle number per record (experimental)
-# Assign table 'HeElectrons' to variable dset
-#dset=f['HeElectrons']
+
 # Read the attribute/variable 'numPtclsInMacro' from HDF5 metadata and assign
 # its value to ParticleNumber variable
-#ParticleNumber=dset.attrs['numPtclsInMacro']
 
-ParticleNumber=f.root.HeElectrons.attrs.numPtclsInMacro
+
+ParticleNumber=f.root._v_children[array_name_in].attrs.numPtclsInMacro
 # Assign RecordsNumber as n (number of macroparticles in source file)
+
 RecordsNumber=n
 # Simple calculate the number of Particles per Record - NOT sure if it is all right
 ParticlePerRecord=ParticleNumber/RecordsNumber
-# Loop has defualt format (what is in - goes out) NOT good for MASP - therefore commented and ignored by compiler
-#for i in range(n):                                  
-#                out.write("%.8e" %(Electrons[i,0]) + \
-#                "\t%.8e" %(Electrons[i,1]) + \
-#                "\t%.8e" %(Electrons[i,2]) + \
-#                "\t%.8e" %(Electrons[i,3]) + \
-#                "\t%.8e" %(Electrons[i,4]) + \
-#                "\t%.8e" %(Electrons[i,5]) + \
-#                "\t%.8e" %(Electrons[i,6]) + \
-#                "\t%.8e" %(Electrons[i,7]) + "\n")
-
-# Loop adapted to MASP file format: X,Px,Y,Py,Z,Pz, 7 is "something"
 
 
-# Write the whole array in MASP input format X,Px,Y,Py,Z,Pz,Weight_of_Particle
-# This is done in loop - reading each item from Electrons array
-# !!!! WARNING !!!!
 # The Axis Z and X are switched - VSim assumes particles travel along X-axis
 # while in Puffin we need particles to travel along Z-axis
 # X<->Z
