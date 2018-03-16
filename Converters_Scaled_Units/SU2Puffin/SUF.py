f4a4e8c4ca8682eb40725e24272bbde68dd1b7e3
Lawrence Campbell <lawrence.campbell@strath.ac.uk>
2018-03-16 18:15:53 +0000

    Changes listed below:
     - Factored the file reading in SU2Puffin and removed all SI units!!!
       (to avoid possible precision issues)
    
     - SU2Puffin is now a Python function
    
     - To Do - use optional variables in the function (e.g. rho), add the Puffin
       Python class, create SU file classes for use in all the scripts,
       put the new reading function (soon to be a class) in a Python package
       to be read by all the SU2* and *2SU Python scripts, change all
       scripts to Python functions
    
    Lawrence

---
+++ Converters_Scaled_Units/SU2Puffin/SUF.py
@@ -0,0 +1,18 @@
+
+import physConst
+
+def readSUF(fname):
+    f=tables.open_file(fnamein,'r')
+    Electrons=f.root.Particles.read()
+    
+# Assign the data to arrays
+
+    x = Electrons[:,0]
+    px = Electrons[:,1]
+    y = Electrons[:,2]
+    py = Electrons[:,3]
+    z = Electrons[:,4]
+    pz = Electrons[:,5]
+    wt = Electrons[:,6]
+    
+    return x, px, y, py, z, pz, wt
